b1e7c39d44afc376ea501bc343a65c0d3b6c48f6
Juan Carbajal <juancarbajal@gmail.com>
2024-01-03 15:40:06 -0500

    without

--- generate-certs.bat
+++
@@ -1,2 +0,0 @@
-mkdir certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/server.key -out certs/server.crt
--- nodejs_app/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Successful response.');
-});
-
-const port= process.env.PORT || "5001";
-app.listen(port, () => console.log('Example app is listening on port '+ port));
